0c3019ad5d997834c36225ec0f2bdedf26052dd2
Taha <dhaileytaha@gmail.com>
2021-01-11 17:06:36 +0800

    search element in Matrix

--- 450/searchMatrix.go
+++ 450/searchMatrix.go
@@ -1,7 +1,8 @@
+
 func searchMatrix(matrix [][]int, target int) bool {
     row, col := 0, len(matrix[0])-1
 
-    for row < len(matrix) && col > 0 {
+    for row < len(matrix) && col >= 0 {
         if matrix[row][col] == target {
             return true
         } else if matrix[row][col] > target {
