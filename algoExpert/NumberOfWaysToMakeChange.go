deee21728195f116ac93f6d8c732112e364adbc1
Taha <dhaileytaha@gmail.com>
2021-02-12 23:30:27 +0800

    NumberOfWaysToMakeChange

--- algoExpert/NumberOfWaysToMakeChange.go
+++ algoExpert/NumberOfWaysToMakeChange.go
@@ -14,6 +14,9 @@ func NumberOfWaysToMakeChange(n int, denoms []int) int {
 	for _, denom := range denoms {
 		for amount := 1; amount < n+1; amount++ {
 			if denom <= amount {
+				//[0,1,2,3,4,5,6] till amount to change i.e. n
+				//[1,0,0,0,0,0,0] initial ways array, is update as per formula
+				//ways[1] = ways[1] + ways[1-1]
 				ways[amount] += ways[amount-denom]
 			}
 		}
